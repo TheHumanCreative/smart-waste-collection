1d0b319d2b1117f5dfbacbff62ab42dca5452413
Miles Lacek <mileslacek@gmail.com>
2019-10-26 12:18:45 -0500

    Added new feature.

---
+++ src/components/SuggestedPage/SuggestedPage.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+
+
+class SuggestedPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Suggested Page</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = reduxStore => {
+  return {
+    reduxStore
+  };
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(SuggestedPage));
